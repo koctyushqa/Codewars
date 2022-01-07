293724235ca71410ec07e8525e41810bd1f1b694
Konstantin <kovucholegich@mail.ru>
2022-01-07 19:16:02 +0300

    Codewars_Python

---
+++ Tasks/03_reverse.py
@@ -0,0 +1,11 @@
+# Complete the solution so that it reverses the string passed into it.
+#
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+def solution(string):
+    return (string[::-1])
+
+print(solution("hello world"))
+#
+# dlrow olleh
